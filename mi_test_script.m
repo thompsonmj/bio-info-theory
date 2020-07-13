749e60aaaf7d6676cfd6919d9094119c7b85f33a
Matthew J. Thompson <thompson.m.j@outlook.com>
2020-07-13 10:13:50 -0400

    Bar plot comparison of Tkacik 2015 vs my calcs

--- mi_test_script.m
+++ mi_test_script.m
@@ -4,29 +4,56 @@ clear
 
 load('gap_data_raw_dorsal_wt_XY-Aligned.mat')
 
-% Y_aligned = Y_yAlign2;
 gene1 = Y_align(:,:,1);
 [nX,nEmbryos] = size(gene1);
 
-% padSize = 0.5*sum(sum(isnan(gene1ppp)))/nEmbryos;
-% idxLow = padSize + 1;
-% idxHigh = nX - padSize;
-% Y_aligned = Y_aligned(idxLow:idxHigh,:,:);
-% gene1 = Y_aligned(:,:,1);
-% [nX,nEmbryos] = size(gene1);
 x = 1/nX:1/nX:1;
 
-% d.Hb = Y_aligned(:,:,1);
 clear d
 
-% data = d.psmad;
 nTrials = 1;
 nBoots = 1e2;
 binCounts = [10:2:50];
 subSamps = [0.50, 0.75, 0.80, 0.85, 0.90, 0.95];
+% subSamps = 0.95
+% binCounts = 50
 
+genes = {'Hb','Kr','Gt','Kni'};
 
 [MI_est] = ...
-    midirectestimate2(Y_align(:,:,:), nTrials, nBoots, binCounts, subSamps);
+    midirectestimate2(Y_align(:,:,[2,4]), nTrials, nBoots, binCounts, subSamps);
 
 extrapolatepi
+
+% while true
+%     beep
+%     pause(2)
+% end
+
+%% Track results
+%Hb: 2.1841, 2.1844, 2.1864
+%Kr: 1.7353, 1.7366
+%Gt: 1.8836, 1.8830
+%Kni: 1.6586, 1.6602
+
+%Hb+Kr: 3.4079
+%Hb+Gt: 3.4037
+%Hb+Kni: 3.1336
+%Kr+Gt: 3.0974
+%Kr+Kni: 2.8909, 2.8929
+%Gt+Kni: 3.2207
+
+%% Plot comparison
+% Fig. 8 | Table 1 | my calc.
+kni = [2.183, 1.83, 1.66]; 
+kr = [2.256, 1.96, 1.74]; 
+gt = [2.079, 1.84, 1.883]; 
+hb = [2.396, 2.24, 2.185];
+
+y = [kni;kr;gt;hb];
+x = categorical({'kni', 'kr', 'gt', 'hb'});
+bar(x,y)
+title('MI Using XY Alignment')
+ylabel('MI (bits)')
+legend('Fig. 8', 'Table 1', 'my calc.')
+grid on
