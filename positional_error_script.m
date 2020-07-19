e100e0df093320feeb8bf1d2946ea90521a066fd
Matthew J. Thompson <thompson.m.j@outlook.com>
2020-07-19 10:39:21 -0400

    Commit to checkout to other branch

--- positional_error_script.m
+++ positional_error_script.m
@@ -2,23 +2,25 @@
 load('gap_data_raw_dorsal_wt_XY-Aligned.mat')
 
 %% Positional error
-[nSamplePts,~,~] = size(Y_align);
-sigma_x = zeros(nSamplePts-1,nGenes);
-figure
-hold on
-for iG = 1:nGenes
-    sigma_x(:,iG) = positionalerror(Y_align(:,:,iG));
-    
-    subplot(2,2,iG)
-    
-    yyaxis left
-    plot(Y_align(:,:,iG),'-')
-    ylabel('Relative Fluorescence')
-    
-    yyaxis right
-    plot(sigma_x(:,iG))
-    ylim([0,0.1])
-    ylabel('Positional Error (\sigma_x/L)')
-    title(genes{iG})
-end
+% [nSamplePts,~,~] = size(Y_align);
+% sigma_x = zeros(nSamplePts-1,nGenes);
+% figure
+% hold on
+% for iG = 1:nGenes
+%     sigma_x(:,iG) = positionalerror(Y_align(:,:,iG));
+%     
+%     subplot(2,2,iG)
+%     
+%     yyaxis left
+%     plot(Y_align(:,:,iG),'-')
+%     ylabel('Relative Fluorescence')
+%     
+%     yyaxis right
+%     plot(sigma_x(:,iG))
+%     ylim([0,0.1])
+%     ylabel('Positional Error (\sigma_x/L)')
+%     title(genes{iG})
+% end
 
+%% PE for multiple genes simultaneously
+sigma_x = positionalerrorn(Y_align);
