511bef93b101f4f7c10459b16d7252b94f829ba7
Matthew J. Thompson <thompson.m.j@outlook.com>
2020-07-24 16:27:40 -0400

    Cleaned positionalerror

--- positionalerror.m
+++ positionalerror.m
@@ -1,13 +1,20 @@
-function sigma_x = positionalerror(profileSet)
+function sigma_x = positionalerror(Y_in)
+%POSITIONALERROR calculates the local positional error for a single
+%profile.
 
-% sigX/L = sigG*abs(dg/dx)^-1
-% profileSet: nX x nSamples (vertical)
+% Input:
+%   > Y_in: size: (number of x-points) x (number of replicates)
+% Output:
+%   > sigma_x: 1-D array of the positional error (not normalized)
 
-g = mean(profileSet,2);
+% Based on Eq. 11 in Dubuis et al. 2013.
+% ref. https://www.itl.nist.gov/div898/handbook/pmc/section5/pmc541.htm
+
+g = mean(Y_in,2);
 
 dgdx = diff(g);
 
-sigG = std(profileSet,1,2);
+sigG = std(Y_in,1,2);
 sigG = sigG(1:end-1);
 
 sigma_x = sigG.*abs(dgdx).^-1/numel(dgdx);
