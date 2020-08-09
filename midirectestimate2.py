8cab8f0a5d7182c96bb48f4892fa350618043690
arrector <rector10@purdue.edu>
2020-08-09 19:07:44 -0400

    waiting for histcounts function

--- midirectestimate2.py
+++ midirectestimate2.py
@@ -2,8 +2,8 @@ import numpy as np
 import random
 import math
 
-def midirectestimate(yData,nTrials,nBoots,binCounts,subSamps):
-
+def midirectestimate2(yData,nTrials,nBoots,binCounts,subSamps):
+    
     nBinsX = yData.shape[0]
     nE = yData.shape[1]
     nG = yData.shape[2]
@@ -28,7 +28,8 @@ def midirectestimate(yData,nTrials,nBoots,binCounts,subSamps):
     for iTrial in range(0,nTrials):
         
         MI_est[iTrial] = {'binSizes':binCounts,'subSamps':subSamps,'nBoots':nBoots,'naiveEst_means': (np.zeros(nBinSizes,nSubsamplesets)),'naiveEst_stds':(np.zeros(nBinSizes,nSubsamplesets))}
-
+        
+    
         if nTrials > 1:
             print("Beginning trial " + str(iTrial) + "/" + str(nBinSizes))
             #tic
@@ -49,14 +50,16 @@ def midirectestimate(yData,nTrials,nBoots,binCounts,subSamps):
             
             for iSubsampleIdx in range(0, nSubsamplesets):
                 mSubsamps = m[iSubsampleIdx]
-                pg = np.zeros(1, nBinsG)
-                pgxJ = np.zeros(nBinsX, nBinsG)
+                pg = np.zeros(nBinsG, nBinsG)
+                pgxJ = np.zeros(nBinsX, nBinsG,nBinsG)
                 MIEq24 = np.zeros(nBoots, 1)
                 for iBoot in range(0, nBoots):
                     subIdx = random.choices(embryoIdcs, weights=None, k=mSubsamps)
-                    sub = yData[:,subIdx]
+                    sub1 = yData[:,subIdx, 1]
+                    sub2 = yData[:,subIdx, 2]
                     
-                    sub = np.reshape(sub, np.size(sub))
+                    sub1 = np.reshape(sub1, np.size(sub1))
+                    sub2 = np.reshape(sub2, np.size(sub2))
                     
                     xMat = np.tile(x.conj().transpose(), (1, mSubsamps))
                     xColumn = np.reshape(xMat, np.size(xMat))
@@ -66,21 +69,25 @@ def midirectestimate(yData,nTrials,nBoots,binCounts,subSamps):
                     #'Normalization','probability'); %joint probability distribution of G and X
                     #Use new histcounts function
                     
+                    
                     pgxJ =pgxJ + eps
+                    pgxJ_perm = np.transpose(pgxJ(2,3,1))
+                    
                     pg = np.sum(pgxJ,1).squeeze()
-                    px = np.tile(pxBin, (nBinsX, nBinsG))
+                    px = np.tile(pxBin, (nBinsX, 1))
                     
                     pg_given_x = pgxJ / px
+                    pg_given_x_perm = np.transpose(pg_given_x,(2,3,1))
                     
                     #Eq. 7
-                    logValue = pg_given_x  * math.log2((pg_given_x /pg))
+                    logValue = pg_given_x_perm  * math.log2((pg_given_x_perm /pg))
                     
-                    MIEq7_temp = np.sum(px * logValue)
+                    MIEq7_temp = np.sum(px * np.sum(logValue,3))
                 
                     MIEq7[iBoot] = MIEq7_temp
                     
                     #Eq. 24
-                    MIEq24_temp = np.sum(pgxJ * math.log2(pgxJ / (px * pg)))
+                    MIEq24_temp = np.sum(pgxJ_perm * math.log2(pgxJ_perm / (px * pg)))
                 
                     MIEq24[iBoot] = MIEq24_temp
                     
@@ -89,15 +96,14 @@ def midirectestimate(yData,nTrials,nBoots,binCounts,subSamps):
                 
             
             if nTrials ==1:
-                print()
+                print(str(round(100*iBinSize/nBinSizes,2)))
                 
         if nTrials>1:
-            print()
+            print(str(100*iTrial/nTrials,2))
             
     #save file
                     
                     
         
     
-
     return MI_est
